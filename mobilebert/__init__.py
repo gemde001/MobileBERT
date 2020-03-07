1020d49704e499a96c3b21d17fa89dd0eccc48c3
gemde001 <gemde001@fiu.edu>
2020-03-06 19:50:39 -0500

    Update __init__.py

--- mobilebert/__init__.py
+++ mobilebert/__init__.py
@@ -8,11 +8,12 @@ import time
 class MobileBERT:
 	def __init__(self):
 		self.max_length = 384
-		self.interpreter = tf.lite.Interpreter(model_path=__file__.replace("__init__.py","mobilebert_float_20191023.tflite"))
 		self.tokenizer = bert.bert_tokenization.FullTokenizer(__file__.replace("__init__.py","vocab.txt"), True)
-		self.interpreter.allocate_tensors()
-		self.input_details = self.interpreter.get_input_details()
-		self.output_details = self.interpreter.get_output_details()
+		with tf.device('/CPU:0'):
+			self.interpreter = tf.lite.Interpreter(model_path=__file__.replace("__init__.py","mobilebert_float_20191023.tflite"))
+			self.interpreter.allocate_tensors()
+			self.input_details = self.interpreter.get_input_details()
+			self.output_details = self.interpreter.get_output_details()
 
 	def get_summary(self):
 		print("Inputs:",self.input_details,"\nOutputs:",self.output_details)
@@ -34,7 +35,6 @@ class MobileBERT:
 				current_segment_id = 1
 		return np.asarray(segments + [0] * (self.max_length - len(tokens)))
 
-
 	def get_ids(self,tokens):
 		token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
 		input_ids = token_ids + [0] * (self.max_length-len(token_ids))
@@ -44,29 +44,42 @@ class MobileBERT:
 		text = text.lower().replace("-"," ")
 		return ["[CLS]"] + self.tokenizer.tokenize(text) + ["[SEP]"]
 
-	def run(self,query,context):
-		stokens =  self.compile_text(query) + self.compile_text(context)
-
-		if len(stokens)>self.max_length:
-			raise IndexError("Token length more than max seq length!")
-			print("Max exceeded")
-		input_ids = tf.dtypes.cast(self.get_ids(stokens),tf.int32)
-		input_masks = tf.dtypes.cast(self.get_masks(stokens),tf.int32)
-		input_segments = tf.dtypes.cast(self.get_segments(stokens),tf.int32)
-
-		self.interpreter.set_tensor(self.input_details[0]['index'], [input_ids])
-		self.interpreter.set_tensor(self.input_details[1]['index'], [input_masks])
-		self.interpreter.set_tensor(self.input_details[2]['index'], [input_segments])
-		sTime = time.time()
+	def getEmbedding(self,string):
 		with tf.device('/CPU:0'):
+			stokens =  self.compile_text(string)
+			if len(stokens)>self.max_length:
+				raise IndexError("Token length more than max seq length!")
+				print("Max exceeded")
+			input_ids = tf.dtypes.cast(self.get_ids(stokens),tf.int32)
+			input_masks = tf.dtypes.cast(self.get_masks(stokens),tf.int32)
+			input_segments = tf.dtypes.cast(self.get_segments(stokens),tf.int32)
+
+			self.interpreter.set_tensor(self.input_details[0]['index'], [input_ids])
+			self.interpreter.set_tensor(self.input_details[1]['index'], [input_masks])
+			self.interpreter.set_tensor(self.input_details[2]['index'], [input_segments])
 			self.interpreter.invoke()
-		print(time.time()-sTime)
-		end_logits = self.interpreter.get_tensor(self.output_details[0]['index'])
-		start_logits = self.interpreter.get_tensor(self.output_details[1]['index'])
+			end_logits = self.interpreter.tensor(self.output_details[0]['index'])()
+			start_logits = self.interpreter.tensor(self.output_details[1]['index'])()
+		return {"start":start_logits,"end":end_logits}
 
-		end = tf.argmax(end_logits,output_type=tf.dtypes.int32).numpy()[0]
-		start = tf.argmax(start_logits,output_type=tf.dtypes.int32).numpy()[0]
+	def run(self,query,context):
+		with tf.device('/CPU:0'):
+			stokens =  self.compile_text(query) + self.compile_text(context)
+			if len(stokens)>self.max_length:
+				raise IndexError("Token length more than max seq length!")
+				print("Max exceeded")
+			input_ids = tf.dtypes.cast(self.get_ids(stokens),tf.int32)
+			input_masks = tf.dtypes.cast(self.get_masks(stokens),tf.int32)
+			input_segments = tf.dtypes.cast(self.get_segments(stokens),tf.int32)
 
+			self.interpreter.set_tensor(self.input_details[0]['index'], [input_ids])
+			self.interpreter.set_tensor(self.input_details[1]['index'], [input_masks])
+			self.interpreter.set_tensor(self.input_details[2]['index'], [input_segments])
+			self.interpreter.invoke()
+			end_logits = self.interpreter.tensor(self.output_details[0]['index'])()
+			start_logits = self.interpreter.tensor(self.output_details[1]['index'])()
+			end = tf.math.argmax(end_logits,output_type=tf.dtypes.int32,axis=1).numpy()[0]
+			start = tf.math.argmax(start_logits,output_type=tf.dtypes.int32,axis=1).numpy()[0]
 		answers = " ".join(stokens[start:end+1]).replace("[CLS]","").replace("[SEP]","").replace(" ##","")
 		return answers
 		
@@ -87,8 +100,7 @@ if __name__ == "__main__":
 	last = ""
 	for x in range(0,9):
 		sTime = time.time()
-		last = m.run("what year was the declaration of independence signed","n fact, independence was formally declared on July 2, 1776, a date that John Adams believed would be “the most memorable epocha in the history of America.” On July 4, 1776, Congress approved the final text of the Declaration. It wasn't signed until August 2, 1776.")
+		last = m.run("what year was the declaration of independence signed","In fact, independence was formally declared on July 2, 1776, a date that John Adams believed would be “the most memorable epocha in the history of America.” On July 4, 1776, Congress approved the final text of the Declaration. It wasn't signed until August 2, 1776.")
 		avg += (time.time()-sTime)
 	print(str(avg/10)," seconds")
 	print(last)
-
